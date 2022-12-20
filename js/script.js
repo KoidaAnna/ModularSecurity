c1e0eace90656c8796720112c6cc2ea7377127b7
KoidaAnna <104893029+KoidaAnna@users.noreply.github.com>
2022-12-20 15:09:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+AOS.init();
+
+document.getElementById('intro_btn').onclick = function() {
+    document.body.classList.toggle('input-on');
+    document.getElementById('we-section').classList.toggle('input-on');
+    document.getElementById('form-section').classList.toggle('input-on');
+    
+  };
+
+   
